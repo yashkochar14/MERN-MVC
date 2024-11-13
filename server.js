1135d7f070b532a720001b4c6078537fc7486550
Yash Kochar <114456268+yashkochar14@users.noreply.github.com>
2024-11-13 12:38:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const books = require('./routes/books')
 const genre = require('./routes/genre')
 
 const port = 5000
-const mongo_uri = 'mongodb+srv://yashkochar01:yashkochar01@yash.o1jpp.mongodb.net/?retryWrites=true&w=majority&appName=Yash'
+const mongo_uri = 'mongodb+srv://yashkochar01:yashkochar01@yash.o1jpp.mongodb.net/'
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => {
